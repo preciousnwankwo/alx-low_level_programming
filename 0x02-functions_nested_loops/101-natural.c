944156532a6e9b82f56a35a4ec8801bb98174a00
preciousnwankwo <realpreciousnwankwo@gmail.com>
2023-09-12 21:57:51 -0700

    Fix

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,19 +10,19 @@
 
 int main(void)
 {
-  int i = 1;
-  int total = 0;
+	int i = 1;
+	int total = 0;
 
-  while (i < 1024)
-    {
-      if (i % 3 == 0)
-	total += i;
-      else if (i % 5 == 0)
-	total += i;
+	while (i < 1024)
+	{
+		if (i % 3 == 0)
+			total += i;
+		else if (i % 5 == 0)
+			total += i;
 
-      i++;
-    }
-  printf("%d\n", total);
+		i++;
+	}
+	printf("%d\n", total);
 
-  return (0);
+	return (0);
 }

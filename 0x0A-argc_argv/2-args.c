1800020725440dcd48e318f5645a7e0130e7712f
preciousnwankwo <realpreciousnwankwo@gmail.com>
2023-10-22 21:09:34 -0700

    Changes Updated

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,7 +5,7 @@
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *
- * Return = always 0.
+ * Return:  always 0.
  */
 int main(int argc, char *argv[])
 {

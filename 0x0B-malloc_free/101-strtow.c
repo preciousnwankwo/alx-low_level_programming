c761c80ffb1f99d2c25e6f0169b19c35d6c271ec
preciousnwankwo <realpreciousnwankwo@gmail.com>
2023-10-22 21:04:52 -0700

    Changes Updated

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -31,7 +31,7 @@ int count_word(char *s)
  * **strtow - splits a string into words
  * @str: string to split
  *
- * Return = pointer to an array of strings (Success)
+ * Return: pointer to an array of strings (Success)
  * or NULL (Error)
  */
 char **strtow(char *str)
